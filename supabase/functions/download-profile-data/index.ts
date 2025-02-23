34de267ca649f3dcedc0fcb0dfc54d36c03c3af6
sajee_techi <sajeewa@techi.be>
2025-02-23 19:07:22 +0530

    edge function download-profile-data - get comunities with matching dest. - cw SDK throwing error. Continue after fix

--- supabase/functions/download-profile-data/index.ts
+++ supabase/functions/download-profile-data/index.ts
@@ -7,11 +7,31 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 
 import {
     communityConfig,
+    getCommunityConfigsFromUrl,
     type MetadataUpdateData,
 } from "../_citizen-wallet/index.ts";
 import { getServiceRoleClient } from "../_db/index.ts";
 import { upsertProfile } from "../_db/profiles.ts";
 import { getProfileFromId } from "jsr:@citizenwallet/sdk";
+
+/**
+ * Example record:
+ * {
+ *   "data": {
+ *     "topic": "0xf8e1a15aba9398e019f0b49df1a4fde98ee17ae345cb5f6b5e2c27f5033e8ce7",
+ *     "_tokenId": "187958928820613595928785719286779462408251654092"
+ *   },
+ *   "dest": "0x56Cc38bDa01bE6eC6D854513C995f6621Ee71229",
+ *   "hash": "0x95cb5578a8061ae4e8b41bd0859491ebd957181d6b27ea96392976d2d737ed75",
+ *   "nonce": 0,
+ *   "value": "0",
+ *   "sender": "",
+ *   "status": "success",
+ *   "tx_hash": "0x131774b7d3ecdad2e75cb2e388b8a5ed592b05e5f3982383badff711c3326140",
+ *   "created_at": "2025-02-23T12:26:10",
+ *   "updated_at": "2025-02-23T12:26:11.705259"
+ * }
+ */
 
 Deno.serve(async (req) => {
     const { record } = await req.json();
@@ -30,15 +50,33 @@ Deno.serve(async (req) => {
             { status: 400 },
         );
     }
+    const profileContract = dest.toLowerCase();
 
     const community = communityConfig();
+    const communityConfigs = await getCommunityConfigsFromUrl();
 
-    if (
-        dest.toLowerCase() !== community.community.profile.address.toLowerCase()
-    ) {
-        return new Response("Only process profile updates", {
-            status: 200,
-        });
+    if (communityConfigs.length === 0) {
+        return new Response("No community configs found", { status: 400 });
+    }
+
+    const communityMap = new Map(
+        communityConfigs.map((config) => [
+            config.config.community.profile.address.toLowerCase(),
+            config,
+        ]),
+    );
+
+    const communitiesWithDest = Array.from(communityMap.entries())
+        .filter(([address]) => address === dest.toLowerCase())
+        .map(([_, config]) => config);
+
+    if (communitiesWithDest.length === 0) {
+        return new Response(
+            `No community found with profile contract ${profileContract}`,
+            {
+                status: 400,
+            },
+        );
     }
 
     if (status !== "success") {
