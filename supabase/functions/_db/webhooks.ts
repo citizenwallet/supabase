df20118240c4277c9361f26406df40f257114521
Randika Dilshan <rdilshan077788@gmail.com>
2025-05-08 14:26:12 +0530

    Enhance webhook functions with TypeScript interfaces and return types

--- supabase/functions/_db/webhooks.ts
+++ supabase/functions/_db/webhooks.ts
@@ -1,13 +1,34 @@
-import type { SupabaseClient } from "jsr:@supabase/supabase-js@2";
+import type {
+  SupabaseClient,
+  PostgrestResponse,
+} from "jsr:@supabase/supabase-js@2";
 
 const WEBHOOKS_TABLE = "webhooks";
 const WEBHOOK_SECRETS_TABLE = "webhook_secrets";
+
+export interface Webhook {
+  id: string;
+  created_at: Date;
+  updated_at: Date;
+  name: string;
+  url: string;
+  event_contract: string;
+  event_topic: string;
+  alias: string;
+}
+
+export interface WebhookSecret {
+  id: string;
+  created_at: Date;
+  alias: string;
+  secret: string;
+}
 
 export const getWebhook = async (
   client: SupabaseClient,
   event_contract: string,
   event_topic: string
-) => {
+): Promise<PostgrestResponse<Webhook>> => {
   return await client
     .from(WEBHOOKS_TABLE)
     .select("*")
@@ -18,7 +39,7 @@ export const getWebhook = async (
 export const getWebhookSecret = async (
   client: SupabaseClient,
   alias: string
-) => {
+): Promise<PostgrestResponse<WebhookSecret>> => {
   return await client
     .from(WEBHOOK_SECRETS_TABLE)
     .select("*")
