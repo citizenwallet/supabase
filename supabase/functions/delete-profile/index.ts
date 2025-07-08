96644245b3ed32c216ac8822559993c6d980df47
Randika Dilshan <rdilshan077788@gmail.com>
2025-07-08 11:36:30 +0530

    added ProfileDeleteData interface and improved code readability in upsertProfile and delete-profile functions.

--- supabase/functions/delete-profile/index.ts
+++ supabase/functions/delete-profile/index.ts
@@ -1,5 +1,8 @@
+import { tokenTransferEventTopic } from "npm:@citizenwallet/sdk";
+import { isAddress } from "npm:ethers";
+import { ProfileDeleteData } from "../_citizen-wallet/index.ts";
 import { getServiceRoleClient } from "../_db/index.ts";
-import { deleteProfile, getProfileFromId } from "../_db/profiles.ts";
+import { deleteProfile, getProfile } from "../_db/profiles.ts";
 
 // INSERT INTO "public"."t_logs_42220" ("hash", "tx_hash", "created_at", "updated_at", "nonce", "sender", "dest", "value", "data", "status") VALUES ('0x72b54c59a696250f0354560ea7fd17e1c29eb0d0cd20aca4e42253de66f694b3', '0xda039f2f614aa0df006fb223b4b5b04341af492d34e5abcb1a8d13f6a146b11c', '2025-07-07 11:41:37', '2025-07-07 11:41:36.324424', '0', '', '0x8dA817724Eb6A2aA47c0F8d8b8A98b9B3C2Ddb68', '0', '{"to": "0x0000000000000000000000000000000000000000", "from": "0xFe954e73Db802cE6bD63f67B82cb8Df64C73d8Ac", "topic": "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", "tokenId": "1453413301006377630992371589485139822566417094828"}', 'success');
 
@@ -26,17 +29,15 @@ import { deleteProfile, getProfileFromId } from "../_db/profiles.ts";
 }
 */
 
-
 Deno.serve(async (req) => {
     const { record } = await req.json();
 
+    const { dest, status, data } = record;
     console.log("this one record", record);
 
     if (!record || typeof record !== "object") {
         return new Response("Invalid record data", { status: 400 });
     }
-
-    const { dest, status, data } = record;
 
     if (!dest || typeof dest !== "string") {
         return new Response(
@@ -44,7 +45,6 @@ Deno.serve(async (req) => {
             { status: 400 },
         );
     }
-    const profileContract = dest;
 
     if (status !== "success") {
         return new Response("Transaction status is not success, ignoring", {
@@ -52,23 +52,37 @@ Deno.serve(async (req) => {
         });
     }
 
-    if (
-        data.topic !==
-            "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
-    ) {
+    if (data.topic !== tokenTransferEventTopic) {
         return new Response("Transaction topic is not valid, ignoring", {
             status: 200,
         });
     }
 
+    if (data.to != "0x0000000000000000000000000000000000000000") {
+        return new Response("Transaction to is not valid, ignoring", {
+            status: 200,
+        });
+    }
+
+    if (
+        data.from == "0x0000000000000000000000000000000000000000" ||
+        !isAddress(data.from)
+    ) {
+        return new Response("Transaction from is not valid, ignoring", {
+            status: 200,
+        });
+    }
+
+    const profileContract = dest;
+
     // Initialize Supabase client with service role key
     const supabaseClient = getServiceRoleClient();
 
-    const metadataUpdateData = data;
+    const metadataUpdateData = data as ProfileDeleteData;
 
-    const profile = await getProfileFromId(
+    const profile = await getProfile(
         supabaseClient,
-        metadataUpdateData.tokenId,
+        metadataUpdateData.from,
         profileContract,
     );
 
@@ -77,7 +91,7 @@ Deno.serve(async (req) => {
         return new Response("profile not found, ignoring", { status: 200 });
     }
 
-    console.log("profile", profile.data);
+    console.log("profile", profile);
 
     const result = await deleteProfile(
         supabaseClient,
